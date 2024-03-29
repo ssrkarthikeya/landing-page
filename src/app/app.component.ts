bae2f396bbc50afac1e532914bc12cd2564981e5
Sai Sri Rama Karthikeya <ssr.karthikeya@gmail.com>
2024-03-30 01:36:18 +0530

    fix(UI): Mobile first approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { AboutTypewriterComponent } from './about-typewriter/about-typewriter.component';
 import { HeadShotComponent } from './head-shot/head-shot.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AboutTypewriterComponent, HeadShotComponent],
+  imports: [RouterOutlet, HeaderComponent, AboutTypewriterComponent, HeadShotComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

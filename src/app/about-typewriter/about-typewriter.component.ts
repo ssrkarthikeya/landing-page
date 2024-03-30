3cc265e0b39b782a15637bddcaedd40b2b20b421
Sai Sri Rama Karthikeya <ssr.karthikeya@gmail.com>
2024-03-30 09:37:36 +0530

    fix(UI): Links, Responsiveness

--- src/app/about-typewriter/about-typewriter.component.ts
+++ src/app/about-typewriter/about-typewriter.component.ts
@@ -9,33 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutTypewriterComponent implements OnInit {
 
-  jobTitles: string[] = ["UX/UI Developer", "DevOps Engineer", "ML Enthusiast"]; // Add your job titles here
-  text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+  jobTitles: string[] = ["Hi!",
+    "I'm a Full Stack Developer At IBM",
+    "proficient in Spring Boot with Java and Angular with Typescript",
+    "I excel in DevOps with tools like Kubernetes, Jenkins, and Docker, and have expertise in AWS, Azure, and IBM Cloud",
+    "I'm also passionate about ML & Deep Learning"]; // Add your job titles here
+
+  currentText = "Hi!";
   typingSpeed = 50; // Adjust typing speed (milliseconds per character)
   typingAnimation: string = '';
   isRoleComplete: boolean = false;
+  jobIndex: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
-      this.type();
+    this.type();
   }
 
   type() {
-    let charIndex = 0;
-    const textLength = this.text.length;
     const typingAnimationInterval = this.typingSpeed;
 
     const typeCharacter = () => {
-      if (charIndex < textLength) {
-        this.typingAnimation += this.text.charAt(charIndex);
-        charIndex++;
-        setTimeout(typeCharacter, typingAnimationInterval);
-      } else {
-        this.isRoleComplete = true;
-      }
-    };
+      const text = this.jobTitles[this.jobIndex];
+      let charIndex = 0;
+      const textLength = text.length;
 
+      const typeNextCharacter = () => {
+        if (charIndex < textLength) {
+          this.typingAnimation = text.substring(0, charIndex + 1);
+          charIndex++;
+          setTimeout(typeNextCharacter, typingAnimationInterval);
+        } else {
+          setTimeout(() => {
+            this.jobIndex = (this.jobIndex + 1) % this.jobTitles.length; // Loop through jobTitles array
+            this.type(); // Start typing next job title
+          }, 2000); // Wait for 1 second before starting next job title
+        }
+      };
+
+      typeNextCharacter();
+    };
 
     typeCharacter();
   }
